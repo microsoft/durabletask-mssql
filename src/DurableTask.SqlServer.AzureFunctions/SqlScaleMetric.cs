376d52705c7f51fd856ab1c51d6fab5cfa0071fd
Chris Gillum <cgillum@microsoft.com>
2021-04-07 17:04:27 -0700

    Fix IScaleProvider implementation

---
+++ src/DurableTask.SqlServer.AzureFunctions/SqlScaleMetric.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace DurableTask.SqlServer.AzureFunctions
+{
+    using Microsoft.Azure.WebJobs.Host.Scale;
+
+    class SqlScaleMetric : ScaleMetrics
+    {
+        public int RecommendedReplicaCount { get; set; }
+    }
+}
--- src/DurableTask.SqlServer.AzureFunctions/SqlScaleMonitor.cs
+++ src/DurableTask.SqlServer.AzureFunctions/SqlScaleMonitor.cs
@@ -4,6 +4,7 @@
 namespace DurableTask.SqlServer.AzureFunctions
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
@@ -12,7 +13,7 @@ namespace DurableTask.SqlServer.AzureFunctions
     /// <summary>
     /// Azure Functions scale monitor implementation for the Durable Functions SQL backend.
     /// </summary>
-    class SqlScaleMonitor : IScaleMonitor
+    class SqlScaleMonitor : IScaleMonitor<SqlScaleMetric>
     {
         static readonly ScaleStatus ScaleInVote = new ScaleStatus { Vote = ScaleVote.ScaleIn };
         static readonly ScaleStatus NoScaleVote = new ScaleStatus { Vote = ScaleVote.None };
@@ -32,7 +33,10 @@ namespace DurableTask.SqlServer.AzureFunctions
         public ScaleMonitorDescriptor Descriptor { get; }
 
         /// <inheritdoc />
-        public async Task<ScaleMetrics> GetMetricsAsync()
+        async Task<ScaleMetrics> IScaleMonitor.GetMetricsAsync() => await this.GetMetricsAsync();
+
+        /// <inheritdoc />
+        public async Task<SqlScaleMetric> GetMetricsAsync()
         {
             // GetRecommendedReplicaCountAsync will write a trace if the recommendation results
             // in a worker count that is different from the worker count we pass in as an argument.
@@ -44,21 +48,28 @@ namespace DurableTask.SqlServer.AzureFunctions
         }
 
         /// <inheritdoc />
-        public ScaleStatus GetScaleStatus(ScaleStatusContext context)
+        ScaleStatus IScaleMonitor.GetScaleStatus(ScaleStatusContext context) => 
+            this.GetScaleStatusCore(context.WorkerCount, context.Metrics.Cast<SqlScaleMetric>());
+
+        /// <inheritdoc />
+        public ScaleStatus GetScaleStatus(ScaleStatusContext<SqlScaleMetric> context) =>
+            this.GetScaleStatusCore(context.WorkerCount, context.Metrics);
+
+        ScaleStatus GetScaleStatusCore(int currentWorkerCount, IEnumerable<SqlScaleMetric> metrics)
         {
-            SqlScaleMetric? mostRecentMetric = context.Metrics.LastOrDefault() as SqlScaleMetric;
+            SqlScaleMetric? mostRecentMetric = metrics.LastOrDefault();
             if (mostRecentMetric == null)
             {
                 return NoScaleVote;
             }
 
-            this.previousWorkerCount = context.WorkerCount;
+            this.previousWorkerCount = currentWorkerCount;
 
-            if (mostRecentMetric.RecommendedReplicaCount > context.WorkerCount)
+            if (mostRecentMetric.RecommendedReplicaCount > currentWorkerCount)
             {
                 return ScaleOutVote;
             }
-            else if (mostRecentMetric.RecommendedReplicaCount < context.WorkerCount)
+            else if (mostRecentMetric.RecommendedReplicaCount < currentWorkerCount)
             {
                 return ScaleInVote;
             }
@@ -66,11 +77,6 @@ namespace DurableTask.SqlServer.AzureFunctions
             {
                 return NoScaleVote;
             }
-        }
-
-        class SqlScaleMetric : ScaleMetrics
-        {
-            public int RecommendedReplicaCount { get; set; }
         }
     }
 }
