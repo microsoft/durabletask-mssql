bd8b1d4aa1fcc7512eca82d8545ddfb80743900d
Jacob Viau <javia@microsoft.com>
2023-01-05 10:54:58 -0800

    Add dotnet-isolated extensions package

---
+++ src/Functions.Worker.Extensions.DurableTask.SqlServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.Functions.Worker.Extensions.Abstractions;
+
+// This must be updated when updating the version of the package
+[assembly: ExtensionInformation("Microsoft.Azure.DurableTask.SqlServer.AzureFunctions", "1.1.0")]
