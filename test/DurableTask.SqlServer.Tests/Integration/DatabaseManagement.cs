ded8eb506a8fc0009d286533d1207c4d62367924
Chris Gillum <cgillum@microsoft.com>
2024-04-05 15:10:25 -0700

    Update version check test

--- test/DurableTask.SqlServer.Tests/Integration/DatabaseManagement.cs
+++ test/DurableTask.SqlServer.Tests/Integration/DatabaseManagement.cs
@@ -503,8 +503,8 @@ namespace DurableTask.SqlServer.Tests.Integration
                 database.ConnectionString,
                 schemaName);
             Assert.Equal(1, currentSchemaVersion.Major);
-            Assert.Equal(2, currentSchemaVersion.Minor);
-            Assert.Equal(3, currentSchemaVersion.Patch);
+            Assert.Equal(3, currentSchemaVersion.Minor);
+            Assert.Equal(0, currentSchemaVersion.Patch);
         }
 
         sealed class TestDatabase : IDisposable
